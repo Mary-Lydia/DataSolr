2ac79a073c6a5adbcbb2ea9d2c37b066029590db
Mary-Lydia <lydiapio88@gmail.com>
2018-11-21 15:55:41 +0530

    First Commit

---
+++ SpringBootSolr1Run/src/main/java/com/sample/solr/SpringBootSolr1Application.java
@@ -0,0 +1,12 @@
+package com.sample.solr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSolr1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSolr1Application.class, args);
+	}
+}
---
+++ SpringBootSolr1Run/src/test/java/com/sample/solr/SpringBootSolr1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sample.solr;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootSolr1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
